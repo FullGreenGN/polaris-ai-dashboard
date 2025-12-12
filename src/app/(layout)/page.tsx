cc050500a7ec3cada06e94c338515f9e7146846f
fullgreen <j.luc@polarisdev.fr>
2025-12-12 11:46:43 +0100

    feat: add CallToAction component and update font settings

--- src/app/(layout)/page.tsx
+++ src/app/(layout)/page.tsx
@@ -1,8 +1,9 @@
 import Hero from "@/components/3d/HeroScene";
+import {CallToAction} from "@/features/layout/landing/cta";
 
 export default function LandingPage() {
     return (
-        <div className="w-screen h-screen flex flex-col relative">
+        <>
             <Hero
                 title="Where algorithms become art."
                 description="A minimal hero with a neural canvas — crisp, elegant, and quietly expressive. Built with React, Three.js, and a custom CPPN shader."
@@ -14,6 +15,8 @@ export default function LandingPage() {
                 ]}
                 microDetails={["Low‑weight font", "Tight tracking", "Subtle motion"]}
             />
-        </div>
+            <CallToAction/>
+
+        </>
     );
 }
